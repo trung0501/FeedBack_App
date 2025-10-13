e70f25357217d8766b927ef3317ea49b0cc1b72d
trung0501 <nguyentrungt151@gmail.com>
2025-10-13 11:24:34 +0700

    restructure workspace app

--- wefeed/backend/apps/workspacemembers/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import WorkspaceMember
-
-class WorkspaceMemberSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = WorkspaceMember
-        fields = '__all__'
