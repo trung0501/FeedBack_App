7be3eadd5bfee2040d6d4643a23cf25e701a8968
trung0501 <nguyentrungt151@gmail.com>
2025-10-13 10:18:33 +0700

    fix route workspaces

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -42,7 +42,7 @@ function App() {
           <Route path="*" element={<Navigate to="/dashboard" replace />} />
         </Routes>
       </AuthProvider>
-    </BrowserRouter>
+    </BrowserRouter> 
   );
 }
 
